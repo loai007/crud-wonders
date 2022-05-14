d52722293cd938920246ae3dae07898752c7abc6
loai abed alhi <loai007thbk@hotmail.com>
2022-05-14 21:20:19 +0300

    Learning CRUD

--- dist/main.js
+++ dist/main.js
@@ -1,28 +1,48 @@
-const source = $("#wonders-template").html()
-const template = Handlebars.compile(source)
+const source = $("#wonders-template").html();
+const template = Handlebars.compile(source);
 
-const render = function(wonders){
-    $("#wonders").empty()
-    let newHtml = template({wonders})
-    $("#wonders").append(newHtml)
-}
+const render = function (wonders) {
+  $("#wonders").empty();
+  let newHtml = template({ wonders });
+  $("#wonders").append(newHtml);
+};
 
-const fetch = function(){
-    $.get("/wonders", function(response){
-        render(response)
-    })
-}
+const fetch = function () {
+  $.get("/wonders", function (response) {
+    render(response);
+  });
+};
 
-const addWonder = function(){
-    let newWonder = $("#new-wonder-input").val()
-    let newLocation = $("#new-location-input").val()
-    //POST the newWonder to the server
-}
+const addWonder = function () {
+  let newWonder = $("#new-wonder-input").val();
+  let newLocation = $("#new-location-input").val();
+  let data = { name: newWonder, location: newLocation };
+  $.post("/wonder", data, function (response) {
+    console.log("POST complete");
+    fetch();
+  });
+};
 
-$("#wonders").on("click", ".visit", function(){
-    let wonder = $(this).closest(".wonder").find(".name").text()
-    //PUT this to the server: update the wonder's `visited` status to `true`
-})
+const updateVisited = function (wonder) {
+  $.ajax({
+    url: `wonder/${wonder}`,
+    method: "PUT",
+    success: function (response) {
+      console.log("PUT complete");
+    },
+  });
+};
 
+$("#wonders").on("click", ".visit", function () {
+  let wonder = $(this).closest(".wonder").find(".name").text();
+  updateVisited(wonder.split("-")[0].trim());
+  fetch();
+});
 
-fetch() //load the data on page load
+$.ajax({
+  url: "/wonder/Colosseum",
+  method: "DELETE",
+  success: function () {},
+});
+
+fetch(); //load the data on page load
--- server.js
+++ server.js
@@ -1,23 +1,17 @@
-const express = require('express')
-const path = require('path')
+const express = require("express");
+const path = require("path");
+const app = express();
+const api = require("./server/routes/api");
 
-const app = express()
-app.use(express.static(path.join(__dirname, 'dist')))
-app.use(express.static(path.join(__dirname, 'node_modules')))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-const wonders = [
-    { name: "Mount Everest", location: "Nepal", visited: false },
-    { name: "Grand Canyon", location: "Arizona", visited: false },
-    { name: "Botanical Gardens", location: "Singapore", visited: true },
-    { name: "Pantheon", location: "Greece", visited: false },
-    { name: "Colosseum", location: "Italy", visited: true }
-]
+app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, "node_modules")));
 
-app.get('/wonders', function (req, res) {
-    res.send(wonders)
-})
-
-const port = 1337 //because why not
+const port = 1400; //because why not
 app.listen(port, function () {
-    console.log(`Server running on ${port}`)
-})
+  console.log(`Server running on ${port}`);
+});
+
+app.use("/", api);
